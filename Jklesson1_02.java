0330af93deb749ff7c9e6d332e373b8c974b37bb
yosachan <127594209+yosachan@users.noreply.github.com>
2023-04-15 00:26:02 +0900

    Add files via upload

---
+++ Jklesson1_02.java
@@ -0,0 +1,41 @@
+class Jklesson1_02{
+    public static void main(String[] args) {
+        // String[] arr;
+        // arr = new String[3];
+        // arr[0]="sato";
+        // arr[1]="suzuki";
+        // arr[2]="takahashi";
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+        
+        // String[] arr = {"sato", "suzuki", "takahashi"};
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+
+        // String[] arr = {"sato", "suzuki", "takahashi"};
+        // arr[1] = "tanaka";        
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+
+        // String[][] arr;
+        // arr = new String[2][2]; 
+        // arr[0][0]="sato";       
+        // arr[0][1]="suzuki";       
+        // arr[1][0]="tanaka";       
+        // arr[1][1]="takahashi";       
+        // System.out.println(arr[0][0]);
+        // System.out.println(arr[0][1]);
+        // System.out.println(arr[1][0]);
+        // System.out.println(arr[1][1]);
+
+        String[][] arr = {{"sato", "suzuki"}, {"tanaka", "takahashi"}};
+        System.out.println(arr[0][0]);
+        System.out.println(arr[0][1]);
+        System.out.println(arr[1][0]);
+        System.out.println(arr[1][1]);
+
+    }
+}
