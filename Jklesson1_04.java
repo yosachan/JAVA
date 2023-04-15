303c9cf36a4fb59fa4ea896cd12e2e54a42ab44e
yosachan <127594209+yosachan@users.noreply.github.com>
2023-04-16 00:03:52 +0900

    Add files via upload

---
+++ Jklesson1_04.java
@@ -0,0 +1,67 @@
+class Jklesson1_04{
+    public static void main(String[] args){
+        // int age = 22;
+        // // int age = 18;
+        // // if (age >= 20){
+        // //     System.out.println("adult");
+        // // }
+        // if (age >= 20){
+        //     System.out.println("adult");
+        // }else{
+        //     System.out.println("child");
+        // }
+
+        // int age = 0;
+        // if(age >= 20){
+        //     System.out.println("adult");
+        // }else if(age == 0){
+        //     System.out.println("baby");
+        // }else{
+        //     System.out.println("chile");
+        // }
+
+        // for(int i = 0; i <= 4; i++){
+        //     System.out.println(i);
+        // }
+        // for(int i = 0; i <= 4; i++){
+        //     if(i==3){
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+        // for(int i = 0; i <= 4; i++){
+        //     if(i==3){
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+        // for(int i=0; i<=2; i++){
+        //     for(int j=0; j<=2; j++){
+        //         System.out.println(i + "-" + j);
+        //     }
+        // }
+        // int arr[] = {2,4,6,8,10};
+        // for(int i=0; i<=4; i++){
+        //     System.out.println(arr[i]);
+        //  }
+        int arr[] = {2,4,6,8,10};
+        int sum = 0;
+        for(int i=0; i<=4; i++){
+            sum += arr[i];
+         }
+         System.out.println(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
