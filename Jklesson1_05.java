c58a9cabc66b84f699d91f63a885ae2abf0070a0
yosachan <127594209+yosachan@users.noreply.github.com>
2023-04-17 00:35:34 +0900

    Add files via upload

---
+++ Jklesson1_05.java
@@ -0,0 +1,20 @@
+class Student{
+    String name = "sato";
+    public void calculateAvg(int math, int english){
+        System.out.println((math + english) / 2);
+    }
+}
+
+public class Jklesson1_05{
+    public static void main(String[] args) {
+        Student a001 = new Student();
+        a001.name = "sato";
+        System.out.println(a001.name);
+        a001.calculateAvg(90,80);
+
+        Student a002 = new Student();
+        a002.name = "suzuki";
+        System.out.println(a002.name);
+        a002.calculateAvg(60,80);
+    }
+}
